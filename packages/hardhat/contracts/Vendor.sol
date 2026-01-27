4f175ec2c05f8e8bd7bc348b796a4114a020821d
foreverxin <foreverxin26@gmail.com>
2026-01-28 00:41:50 +0800

    Step 1-4

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -9,11 +9,18 @@ contract Vendor is Ownable {
     /// Errors //////
     /////////////////
 
-    // Errors go here...
+    error InvalidEthAmount();
+    error InsufficientVendorTokenBalance(uint256 available, uint256 required);
+    error EthTransferFailed(address to, uint256 amount);
+    error InvalidTokenAmount();
+    error InsufficientVendorEthBalance(uint256 available, uint256 required);
+    event SellTokens(address indexed seller, uint256 amountOfTokens, uint256 amountOfETH);
 
     //////////////////////
     /// State Variables //
     //////////////////////
+
+    uint256 public constant tokensPerEth = 100;
 
     YourToken public immutable yourToken;
 
@@ -21,7 +28,7 @@ contract Vendor is Ownable {
     /// Events /////
     ////////////////
 
-    // Events go here...
+    event BuyTokens(address indexed buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
     ///////////////////
     /// Constructor ///
@@ -35,9 +42,51 @@ contract Vendor is Ownable {
     /// Functions /////
     ///////////////////
 
-    function buyTokens() external payable {}
+    function buyTokens() external payable {
+        if (msg.value == 0) {
+            revert InvalidEthAmount();
+        }
 
-    function withdraw() public onlyOwner {}
+        uint256 amountToBuy = msg.value * tokensPerEth;
 
-    function sellTokens(uint256 amount) public {}
+        uint256 vendorBalance = yourToken.balanceOf(address(this));
+        if (vendorBalance < amountToBuy) {
+            revert InsufficientVendorTokenBalance({
+                available: vendorBalance,
+                required: amountToBuy
+            });
+        }
+
+        bool sent = yourToken.transfer(msg.sender, amountToBuy);
+        require(sent, "Failed to transfer tokens to user");
+
+        emit BuyTokens(msg.sender, msg.value, amountToBuy);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 ownerBalance = address(this).balance;
+        (bool sent, ) = payable(owner()).call{value: ownerBalance}("");
+        if (!sent) {
+            revert EthTransferFailed(owner(), ownerBalance);
+        }
+    }
+
+    function sellTokens(uint256 amount) public {
+        if (amount == 0) {
+            revert InvalidTokenAmount();
+        }
+        uint256 amountOfEthToTransfer = amount / tokensPerEth;
+        if (address(this).balance < amountOfEthToTransfer) {
+            revert InsufficientVendorEthBalance({
+                available: address(this).balance,
+                required: amountOfEthToTransfer
+            });
+        }
+        yourToken.transferFrom(msg.sender, address(this), amount);
+        (bool sent, ) = payable(msg.sender).call{value: amountOfEthToTransfer}("");
+        if (!sent) {
+            revert EthTransferFailed(msg.sender, amountOfEthToTransfer);
+        }
+        emit SellTokens(msg.sender, amount, amountOfEthToTransfer);
+    }
 }
